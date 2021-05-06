5d589825a8307db4c55cc5068a768b27e883f159
myajgar <ajgar.ali1987@gmail.com>
2021-05-06 01:29:23 -0400

    initial commit

--- src/datastructure/DataReader.java
+++ src/datastructure/DataReader.java
@@ -1,8 +1,74 @@
 package datastructure;
+import java.io.*;
+import java.io.BufferedReader
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Stack;
 
 public class DataReader {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)throws ClassNotFoundException ,SQLException {
+        FileReader file =null;
+        try{
+            file=new FileReader("/src/data/self-driving-car");
+
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+        BufferedReader buff =new BufferedReader(file);
+        String s = null;
+        String s2=null;
+        String s3 =null;
+        String s4 =null;
+        LinkedList<String> text =new LinkedList<>();
+        Stack<String> stack =new Stack<>();
+        while (true){
+            try{
+                if(!((s=buff.readLine())!=null)) break;
+                System.out.println(s);
+
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+            s2=s.replace("","");
+            s3=s2.replace(" . " , " ");
+            s4 =s3.replace("!","");
+            String arr[] =s4.split("");
+            for(String i : arr){
+                text.add(i);
+                stack.add(i);
+            }
+        }
+        System.out.println("---------the linklist result-------------");
+        System.out.println(text);
+        System.out.println("------------------the stuck result------------");
+        System.out.println("----------------pushing at the end of the stack--------------");
+        stack.push("ali");
+        stack.push("ajgar");
+        stack.push("sumon");
+        System.out.println(stack);
+        System.out.println("------------peek from stack is taking from the last soked one-----------");
+        System.out.println(stack.peek());
+        System.out.println(stack);
+        System.out.println("--------------pooling from stack is poooling from the last one stacked--------");
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack);
+        System.out.println("------------------how to read stack usisnginhanced for loop----------");
+        for (String m : stack){
+            System.out.println(m);
+        }
+        System.out.println("-----------how to use iterator to read stack-----------------");
+        Iterator ita =stack.iterator();
+        while (ita.hasNext()){
+            System.out.println(ita.next());
+        }
         /*
          * User API to read the below textFile and print to console.
          * Use BufferedReader class.
--- src/datastructure/UseMap.java
+++ src/datastructure/UseMap.java
@@ -1,8 +1,56 @@
 package datastructure;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class UseMap {
+    public static void main(String[]args) {
+        ArrayList<String> vs =new ArrayList<>();
+        vs.add("mz");
+        vs.add("it");
+        vs.add("ah");
+        vs.add("aw");
+        vs.add("ig");
+        ArrayList<String> vm = new ArrayList<>();
+        vm.add("tod");
+        vm.add("mod");
+        vm.add("cod");
+        vm.add("lod");
+        ArrayList<String>vl =new ArrayList<>();
+        vl.add("nj");
+        vl.add("ct");
+        vl.add("ny");
 
-    public static void main(String[] args) {
+        HashMap<String ,ArrayList>cm =new HashMap<>();
+        cm.put("stamford" , vs);
+        cm.put("milford" , vm) ;
+        cm.put("lagar" ,vl);
+        System.out.println("-------------how to get specific keyvalue");
+        System.out.println(cm.get("stamford"));
+        System.out.println("----------------how to get the keys-----------------");
+        System.out.println(cm.keySet());
+        System.out.println("-----------------how to get the value of hash");
+        System.out.println(cm.values());
+        System.out.println("_______________how to chek the key if exit or not---------------");
+        System.out.println(cm.containsKey("milford"));
+        System.out.println("----------------how to see the size of the hash---------------");
+        System.out.println(cm.size());
+        System.out.println("-------how to display the hash or how to read the hash---------------");
+        System.out.println(cm);
+        System.out.println("------------------how to get value of the key-----------------------");
+        System.out.println(cm.get("stamford"));
+        System.out.println("-------------how to read the hash with inhanced forloop---------------");
+        for (Map.Entry m : cm.entrySet()) {
+            System.out.println(m.getKey() + "  " + m.getValue());
+            System.out.println("---------------how to read the value of the hash with iterator class-------");
+           Iterator ita = cm.entrySet().iterator();
+            while (ita.hasNext()){
+                System.out.println(ita.next());
+            }
+        }
+
+
         /*
          * Demonstrate how to use Map that includes storing and retrieving elements.
          * Add List<String> into a Map. Like, Map<String, List<string>> list = new HashMap<String, List<String>>();
